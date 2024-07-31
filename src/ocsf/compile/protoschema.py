e92f341deeda5aa218ed03a99e620a4197f7bce4
Jeremy Fisher <106115283+query-jeremy@users.noreply.github.com>
2024-07-31 17:03:21 -0500

    Adding find_base() method

--- src/ocsf/compile/protoschema.py
+++ src/ocsf/compile/protoschema.py
@@ -1,13 +1,15 @@
 import dacite
 
 from copy import deepcopy
-from dataclasses import asdict
+from dataclasses import asdict, dataclass
 from pathlib import PurePath
 from typing import Any, cast, TypeVar
+from enum import IntEnum
 
 from ocsf.schema import OcsfSchema, OcsfObject, OcsfEvent, OcsfType, OcsfProfile, OcsfExtension
 from ocsf.repository import (
     Repository,
+    DefnWithExtends,
     ObjectDefn,
     EventDefn,
     SpecialFiles,
@@ -124,6 +126,29 @@ class ProtoSchema:
                     pass
 
         raise KeyError(f"Extension {name} not found")
+
+    def find_base(self, child: RepoPath, recurse: bool = False) -> RepoPath | None:
+        """Find the path to the base object or event for object or event at a given path."""
+        data = self[child].data
+        if isinstance(data, ObjectDefn):
+            if data.extends is not None:
+                parent = self.find_object(data.extends)
+                assert isinstance(parent.data, ObjectDefn)
+                if parent.data.extends is not None and recurse:
+                    return self.find_base(parent.path)
+                else:
+                    return parent.path
+
+        elif isinstance(data, EventDefn):
+            if data.extends is not None:
+                parent = self.find_event(data.extends)
+                assert isinstance(parent.data, EventDefn)
+                if parent.data.extends is not None and recurse:
+                    return self.find_base(parent.path)
+                else:
+                    return parent.path
+
+        return None
 
     def schema(self) -> OcsfSchema:
         schema = OcsfSchema(version="0.0.0")  # Version updated below
